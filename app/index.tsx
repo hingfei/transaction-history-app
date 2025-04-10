1f7cba9c57fdf296b92450347a2601b50e805fe6
hingfei <soonghingfei@hotmail.com>
2025-04-10 22:16:13 +0800

    config: project setup
    - Added nativewind for styling
    - Cleaned up the project template from expo
    - Added prettier for code formatting

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className={"bg-blue-500 flex-1 justify-center items-center"}
+    >
+      <Text className={"text-5xl font-bold text-white"}>Secure Bank</Text>
+    </View>
+  );
+}
